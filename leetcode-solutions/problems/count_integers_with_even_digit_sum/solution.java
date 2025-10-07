949bb38cdc370b37c33fdf01a0a6d9bb6f515692
Ponabinanth <abinanth574@gmail.com>
2025-10-07 04:06:46 +0000

    Sync LeetCode submission - Count Integers With Even Digit Sum (java)

---
+++ leetcode-solutions/problems/count_integers_with_even_digit_sum/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countEven(int num) {
+        int sum = 0, n = num;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        if (sum % 2 == 0) {
+            return num / 2;
+        } else {
+            return (num - 1) / 2;
+        }
+    }
+}
