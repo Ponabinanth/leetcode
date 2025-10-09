4a06499b37f84b552e50929a34c912164b6b18b6
Ponabinanth <abinanth574@gmail.com>
2025-10-09 09:04:58 +0000

    Sync LeetCode submission - Generate Parentheses (java)

---
+++ leetcode-solutions/problems/generate_parentheses/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        generate("", 0, 0, n, result);
+        return result;
+    }
+    private void generate(String current, int open, int close, int n, List<String> result) {
+        if (current.length() == 2 * n) {
+            result.add(current); 
+            return;
+        }
+        if (open < n) {
+            generate(current + "(", open + 1, close, n, result);
+        }
+        if (close < open) {
+            generate(current + ")", open, close + 1, n, result);
+        }
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.generateParenthesis(3));  
+        System.out.println(solution.generateParenthesis(1)); 
+    }
+}
