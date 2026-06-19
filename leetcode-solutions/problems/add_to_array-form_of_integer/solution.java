7b712ad2a8acf2299b0a7b8a9ba83b1ea689c51b
Ponabinanth <abinanth574@gmail.com>
2026-06-19 05:02:01 +0000

    Sync LeetCode submission - Add to Array-Form of Integer (java)

---
+++ leetcode-solutions/problems/add_to_array-form_of_integer/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer>list=new ArrayList<>();
+        int i = num.length-1;
+        while(i>=0 || k>0)
+        {
+            if(i>=0)
+            {
+                k=num[i]+k;
+            }
+            list.add(k%10);
+            k=k/10;
+            i--;
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
